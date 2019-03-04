4bce35e93686d57bab06f398fd277c08dcdc1aa9
Steven Thornton <Stevenwthornton@Stevens-MBP.attlocal.net>
2019-03-03 23:35:53 -0600

    added content to STORE

---
+++ store.js
@@ -0,0 +1,101 @@
+// this file with store my questions and answers
+
+const STORE = [
+  {
+    question: 'Who currently holds the spot for the greatest golfer of all time, based on total tournament victories?',
+    answers: [
+      'Jack Nicklaus',
+      'Tiger Woods',
+      'Arnold Palmer',
+      'Sam Snead'
+    ],
+    correctAnswer: 'Sam Snead'
+  }, 
+  {
+    question: 'What is an albatross?',
+    answers: [
+      'Accidentally hitting another player’s ball on the green',
+      'Driving onto the fairway of an adjacent hole',
+      'Shooting a score of three under par on a single hole',
+      'Using the flag as a backboard for your putt'
+    ],
+    correctAnswer: 'Shooting a score of three under par on a single hole'
+  },
+  {
+    question: 'What is the record for most tour-tournament victories?',
+    answers: [
+      '82',
+      '80',
+      '73',
+      '64'
+    ],
+    correctAnswer: '82'
+  },
+  {
+    question: 'What is the main difference between red and white stakes?',
+    answers: [
+      'Red stakes require you to always shoot your second shot from the drop zone. White stakes require it from where your ball went out of bounds',
+      'If you can find your ball in red stakes, you can play it without penalty. White stakes require a re-tee plus an additional penalty stroke',
+      'Red stakes are red, white stakes are white',
+      'There is no difference'
+    ],
+    correctAnswer: 'If you can find your ball in red stakes, you can play it without penalty. White stakes require a re-tee plus an additional penalty stroke'
+  },
+  {
+    question: 'As of 2019, you are allowed to keep the flag in the hole if desired.',
+    answers: [
+      'True',
+      'False'
+    ],
+    correctAnswer: 'True'
+  },
+  {
+    question: 'As of 2018, who holds the record for most Major Tournament victories?',
+    answers: [
+      'Jack Nicklaus',
+      'Tiger Woods',
+      'Sam Snead',
+      'Arnold Palmer'
+    ],
+    correctAnswer: 'Jack Nicklaus'
+  },
+  {
+    question: 'From where does the modern game of Golf originate?',
+    answers: [
+      'England',
+      'Rome',
+      'Greece',
+      'Ireland'
+    ],
+    correctAnswer: 'Ireland'
+  },
+  {
+    question: 'What does the term ‘Grounding the club’ refer to?',
+    answers: [
+      'Touching the sand, water, or ground while taking practice swings in a hazard',
+      'Anytime your clubs touch the ground',
+      'The process of taking divots out of the ground while swinging',
+      'Sending your clubs to their room as their punishment for disobeying'
+    ],
+    correctAnswer: 'Touching the sand, water, or ground while taking practice swings in a hazard'
+  },
+  {
+    question: 'What is a score of par on most courses?',
+    answers: [
+      '70',
+      '72',
+      '74',
+      '75'
+    ],
+    correctAnswer: '72'
+  },
+  {
+    question: 'What is a golf handicap?',
+    answers: [
+      'Shooting from tee boxes closer to the pin',
+      'Lowering par for players who are really good',
+      'An average of your last 20 rounds of golf',
+      'Special wheelchairs people use to get around the course'
+    ]
+  }
+]
